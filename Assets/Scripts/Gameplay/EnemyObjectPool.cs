7d963b2ce3283b9eb39c76f7575c307554fa05cf
Erick Subero <suberoerick@gmail.com>
2022-01-29 13:21:33 -0400

    Rename
    - Projectiles new name.

---
+++ Assets/Scripts/Gameplay/EnemyObjectPool.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class EnemyObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObjectCollection mortalProjectiles = default(GameObjectCollection);
+    [SerializeField] private GameObjectCollection hardnessProjectiles = default(GameObjectCollection);
+    [SerializeField] private IntReference projectilesPool = default(IntReference);
+    [SerializeField] private GameObject mortalProjectilePrefab = default(GameObject);
+    [SerializeField] private GameObject hardnessProjectilePrefab = default(GameObject);
+    [SerializeField] private Transform mortalContainer = default(Transform);
+    [SerializeField] private Transform hardnessContainer = default(Transform);
+
+    private void Start()
+    {
+        mortalProjectiles.Clear();
+        hardnessProjectiles.Clear();
+        InstantiateProjectiles();
+    }
+
+    private void InstantiateProjectiles()
+    {
+        for (int i = 0; i < projectilesPool.Value; i++)
+        {
+            GameObject mortal = Instantiate(mortalProjectilePrefab) as GameObject;
+            GameObject hardness = Instantiate(hardnessProjectilePrefab) as GameObject;
+            mortal.GetComponent<Transform>().SetParent(mortalContainer);
+            hardness.GetComponent<Transform>().SetParent(hardnessContainer);
+            mortalProjectiles.Add(mortal);
+            hardnessProjectiles.Add(hardness);
+            mortal.SetActive(false);
+            hardness.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/ProjectileSpitter.cs
+++ Assets/Scripts/Gameplay/ProjectileSpitter.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ProjectileSpitter : MonoBehaviour
 {
-    [SerializeField] private GameObjectCollection type1Projectiles = default(GameObjectCollection);
-    [SerializeField] private GameObjectCollection type2Projectiles = default(GameObjectCollection);
+    [SerializeField] private GameObjectCollection mortalProjectiles = default(GameObjectCollection);
+    [SerializeField] private GameObjectCollection hardnessProjectiles = default(GameObjectCollection);
     [SerializeField] private BoolReference isGameOver = default(BoolReference);
     [SerializeField] private FloatReference minSpawnTime = default(FloatReference);
     [SerializeField] private FloatReference maxSpawnTime = default(FloatReference);
@@ -56,13 +56,13 @@ public class ProjectileSpitter : MonoBehaviour
 
         if (lastProjectileType.Equals(ProjectileType.Type2))
         {
-            for (int i = 0; i < type1Projectiles.Count; i++)
+            for (int i = 0; i < mortalProjectiles.Count; i++)
             {
-                if (!type1Projectiles[i].activeInHierarchy)
+                if (!mortalProjectiles[i].activeInHierarchy)
                 {
-                    type1Projectiles[i].transform.localPosition = initialPosition;
-                    type1Projectiles[i].transform.localRotation = initialRotation;
-                    type1Projectiles[i].SetActive(true);
+                    mortalProjectiles[i].transform.localPosition = initialPosition;
+                    mortalProjectiles[i].transform.localRotation = initialRotation;
+                    mortalProjectiles[i].SetActive(true);
                     lastProjectileType = ProjectileType.Type1;
                     break;
                 }
@@ -70,13 +70,13 @@ public class ProjectileSpitter : MonoBehaviour
         }
         else if (lastProjectileType.Equals(ProjectileType.Type1))
         {
-            for (int i = 0; i < type2Projectiles.Count; i++)
+            for (int i = 0; i < hardnessProjectiles.Count; i++)
             {
-                if (!type2Projectiles[i].activeInHierarchy)
+                if (!hardnessProjectiles[i].activeInHierarchy)
                 {
-                    type2Projectiles[i].transform.localPosition = initialPosition;
-                    type2Projectiles[i].transform.localRotation = initialRotation;
-                    type2Projectiles[i].SetActive(true);
+                    hardnessProjectiles[i].transform.localPosition = initialPosition;
+                    hardnessProjectiles[i].transform.localRotation = initialRotation;
+                    hardnessProjectiles[i].SetActive(true);
                     lastProjectileType = ProjectileType.Type2;
                     break;
                 }
